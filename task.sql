cba082498d7bea819c274ccf365d8f54b09eb443
Oleg <oleggavelya@gmail.com>
2026-01-07 15:42:26 +0200

    Solution

--- task.sql
+++ task.sql
@@ -2,10 +2,17 @@
 USE ShopDB; 
 
 -- Some data should be created outside the transaction (here)
-
+INSERT INTO Orders (CustomerID, date)
+VALUES (1, '2026-01-07');
 -- Start the transaction 
-START TRANSACTION; 
-
--- And some data should be created inside the transaction 
+START TRANSACTION;
+INSERT INTO OrderItems (OrderID, ProductID, Count)
+VALUES (1, 1, 5);
+-- And some data should be created inside the transaction
+UPDATE Products
+INNER JOIN OrderItems
+ON Products.ID = OrderItems.ProductID
+SET Products.WarehouseAmount = Products.WarehouseAmount - OrderItems.Count
+WHERE Products.ID = 1;
 
 COMMIT; 
